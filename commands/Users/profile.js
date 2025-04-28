d50e784ed538ae42eeb30591437fc8ca67253045
MaxedCarp <60354312+MaxedCarp@users.noreply.github.com>
2025-04-28 15:34:24 +0300

    Removed extended profile support

--- commands/Users/profile.js
+++ commands/Users/profile.js
@@ -6,14 +6,6 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('profile')
 		.setDescription('Prints out your profile')
-		.addStringOption(option =>
-            option.setName('mode')
-                .setDescription('Extended mode displays frames and subsumed abilities.')
-				.addChoices(
-                    { name: 'Simple', value: 'simple' },
-                    { name: 'Extended', value: 'extended' }
-				)
-            .setRequired(true))
 		.addUserOption(option =>
             option.setName('user')
                 .setDescription('User to check the profile for')),
@@ -46,46 +38,6 @@ module.exports = {
 				const mode = interaction.options.getString('mode');
 				if (obj.mains.length > 0)
 					exampleEmbed.addFields({ name: `Main Frame${obj.mains.length > 1 ? "s " + "(" + obj.mains.length + ")" : ""}`, value: obj.mains.sort().toString().replaceAll(',', ', ') });
-				if (mode === "extended") {
-					if (obj.framelist.length > 0)
-					{
-						var ls = obj.framelist.filter(a => !a.includes("Prime") && !a.includes("Umbra"));
-						var count = ls.length;
-						var ls2 = ls.sort().toString();
-						let n = 4;
-						let ch = ',';
-
-						let regex = new RegExp("((?:[^" +ch+ "]*" +ch+ "){" + (n-1) + "}[^" +ch+ "]*)" +ch, "g");
-
-						ls2 = ls2.replace(regex, '$1,\n');
-						if (ls.length > 0)
-							exampleEmbed.addFields({ name: `Frames (${ls.length} / ${await colframes.count({name: {$regex: '^[^ ]*$'}})})`, value: ls2.replaceAll(',', ', ') });
-						ls = obj.framelist.filter(a => !!a.includes("Prime") || !!a.includes("Umbra"));
-						testtt = obj.framelist.filter(a => !!a.includes("Excalibur Prime"));
-						ntst = 1;
-						ntst -= testtt.length;
-						count += ls.length;
-						ls2 = ls.sort().toString();
-						ls2 = ls2.replace(regex, '$1,\n');
-						if (ls.length > 0)
-							exampleEmbed.addFields({ name: `Prime Frames (${ls.length} / ${primeFrames.length - ntst})`, value: ls2.replaceAll(',', ', ') });
-						
-						exampleEmbed.addFields({ name: `Total Frame Count`, value: `${count}` });
-					}
-					if (obj.sublist.length > 0)
-					{
-						var ls = obj.sublist;
-						ls = ls.sort().toString();
-						let n = 4;
-						let ch = ',';
-
-						let regex = new RegExp("((?:[^" +ch+ "]*" +ch+ "){" + (n-1) + "}[^" +ch+ "]*)" +ch, "g");
-
-						ls = ls.replace(regex, '$1,\n');
-
-						exampleEmbed.addFields({ name: `Subsumed Abilities (${obj.sublist.length} / ${regFrames.length})`, value: ls.replaceAll(',', ', ') });
-					}
-				}
 				exampleEmbed.setFooter({ text: alli[interaction.applicationId][0], iconURL: alli[interaction.applicationId][1] });
 				await interaction.reply({ embeds: [exampleEmbed] });
 			}
